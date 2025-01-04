ad9f80c5fd0c0b21b47425de91209f5e2319da73
Aaron Anderson <sylverblade16@gmail.com>
2025-01-03 21:28:14 -0600

    Realized I had created an extra script that would've served the same purpose as ScreenEndTrigger.cs

--- Assets/Scripts/ScreenEndTrigger.cs
+++ Assets/Scripts/ScreenEndTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScreenEndTrigger : MonoBehaviour
+public class ScreenEndTrigger : Subject
 {
     // Start is called before the first frame update
     void Start()
